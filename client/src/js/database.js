99281f7800f307df2cb6acc982aa8fcd27469dfb
choyoonme <yoonme@gmail.com>
2022-07-26 20:46:18 -0400

    debug service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,17 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  const db = await initdb();
-  db.put("jate", "content", content);
+export const putDb = async (id, value) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const objStore = tx.objectStore("jate");
+  const req = objStore.put({ id: id, value: value });
+  const res = await req;
 };
-console.error("putDb not implemented");
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  const db = await initdb();
-  return db.get("jate", "content");
+export const getDb = async (value) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const objStore = tx.objectStore("jate");
+  const req = objStore.getAll();
+  const res = await req;
 };
-console.error("getDb not implemented");
 
 initdb();
